ecc6ffd88c09c7a15fc7dc87d07dda26d471ab14
mehaksuri15 <mehaksuri15@gmail.com>
2024-10-27 08:02:28 +0000

    arrays1

---
+++ 01__basics/array.js
@@ -0,0 +1,5 @@
+const myArr=[0,1,2,3,4,5]
+console.log(myArr[0])
+//js array copy operations create shallow copies
+//shallow: a copy whose properties share the same references as those of source obj from which copy was made
+//deep : a copy whose properties share the same references as those of source obj from which copy was made
