20a39abd7259f4845500b66bdbc3be3fcdb86251
mehaksuri15 <mehaksuri15@gmail.com>
2024-10-27 18:59:34 +0000

    object

---
+++ test.js
@@ -0,0 +1,26 @@
+//declaration of string way1 
+console.log("Mehak")
+const accountId = 14453
+let accountEmail = "mehak@google.com"
+var accountPassword = "12345"
+accountCity= "Jaipur"
+
+// accountId=2 gives error as it is constant
+accountEmail="hc@hc.com"
+accountCity = "Delhi"
+accountPassword = "121212"
+
+console.log(accountId); 
+console.table([accountId, accountPassword ,accountCity])
+
+const name = "hitesh"
+const repoCount=50
+
+//modern way
+console.log(`Hello my name is $(name) and my repo count is $(repoCount)`);
+
+//declaration of string way 2
+const gameName=new String('harshit') 
+
+
+
