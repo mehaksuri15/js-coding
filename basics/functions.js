1b5d9ea040ce54610c2351d35d4e0e5648e6a2bf
mehaksuri15 <mehaksuri15@gmail.com>
2024-10-28 08:17:58 +0000

    function

---
+++ basics/functions.js
@@ -0,0 +1,12 @@
+function sayMyName(){
+    console.log("m")
+    console.log("e")
+    console.log("h")
+    console.log("a")
+    console.log("k")   
+}
+sayMyName();
+
+function addTwoNumbers(number1,number2){
+    console.log(number1 + number2) 
+}
