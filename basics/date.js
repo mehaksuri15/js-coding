78fca1d74f6ee580064cc9f13f2fe753728d054c
mehaksuri15 <mehaksuri15@gmail.com>
2024-10-28 17:01:40 +0000

    date

---
+++ basics/date.js
@@ -0,0 +1,29 @@
+let myDate = new Date
+console.log(myDate)
+console.log(myDate.toString())
+console.log(myDate.toJSON())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toUTCString())
+console.log(myDate.toISOString())
+console.log(myDate.toLocaleTimeString())
+console.log(myDate.toTimeString())
+console.log(myDate.toLocaleDateString())
+console.log(typeof myDate)//object
+
+let myCreatedDate = new Date (2023,0,2) //month starts from 0
+console.log(myCreatedDate.toDateString())
+
+
+let myCreatedDate2 = new Date (2023,0,2,5,3) //month starts from 0
+console.log(myCreatedDate2.toLocaleString())
+
+
+let myCreatedDate3 = new Date ("2023-01-14") //month starts from 1 in format of YY-MM-DD
+console.log(myCreatedDate3.toLocaleString())
+
+// The Date.now() static method returns the number of milliseconds elapsed since the epoch, which is defined as the midnight at the beginning of January 1, 1970,
+let myTimeStamp = Date.now()
+console.log(myTimeStamp)
+console.log(myCreatedDate.getTime())
+console.log(Date.now())
