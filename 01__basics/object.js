361d5847f92d031cf566aa6e6238a3709dc1cd3c
mehaksuri15 <mehaksuri15@gmail.com>
2024-10-28 07:05:03 +0000

    object

--- 01__basics/object.js
+++ 01__basics/object.js
@@ -49,4 +49,58 @@ tinderUser.name = "Sam"
 tinderUser.isLoggedIn = false
 
 
+// console.log(tinderUser);
 
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "mehak",
+            lastname: "suri"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+//it creates object ke andar object
+
+// const obj3 = Object.assign({}, obj1, obj2, obj4)
+
+//best way to concatenate obj1 and obj2 using spread operator
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+]
+//means array ke andar objects hai
+
+//method to access
+console.log(users[1].email)
+console.log(tinderUser)
+
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+//gives array in form of key value pairs
+
+//to check if value exists or not
+console.log(tinderUser.hasOwnProperty('name'));
